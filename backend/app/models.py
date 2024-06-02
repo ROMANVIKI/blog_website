7cade2ae4c2840da981a92cff5035a3b26471c95
ROMANVIKI <vikiadhi2016@gmail.com>
2024-06-02 22:51:31 +0530

    createing pages

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Comment by {self.author} on {self.blog}'
+
+class BlogImage(models.Model):
+    blog = models.ForeignKey(Blog, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog_images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Image for {self.blog}'
