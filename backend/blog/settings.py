0ea3222a72ab662eced774f2b40977e72fc0088d
ROMANVIKI <vikiadhi2016@gmail.com>
2024-06-02 22:34:01 +0530

    rm project dir

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s1e(2za25o^z5-r_v5n+6sw$+vf@zaa$^b8js244=l#ru!q!^$'
+SECRET_KEY = 'django-insecure-=sy3ab!mv&6@=2%u)3dzkci0zgh#akg7rq%%x=9%a@3!#bg!(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
